6c243097ed8f7280c1db0a334f368663b0035edd
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-18 20:19:34 -0300

    entendedo o jsx

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Thiago  "
+
+  function sum(a, b){
+    return a+b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
       <h1>Olá React</h1>
+      <p>Olá, {name}</p>
+      <p>{sum(1,2)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
