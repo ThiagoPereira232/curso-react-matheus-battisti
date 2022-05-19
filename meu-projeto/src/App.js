1436e54e673cb6763c7bd1eed8e47bdb1c2f4bc6
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-18 21:06:32 -0300

    fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import HelloWord from './components/HelloWord'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <SayMyName nome="Thiago" />
       <SayMyName nome="Maria" />
       <Pessoa nome="Rodrigo" idade="28" profissao="Programar" foto="https://via.placeholder.com/150" />
+      <List />
     </div>
   );
 }
