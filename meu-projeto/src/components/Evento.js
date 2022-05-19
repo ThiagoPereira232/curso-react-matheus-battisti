a36aad64be41260719d1c785da228a3d294ce141
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-05-19 15:59:56 -0300

    eventos props

--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -1,13 +1,20 @@
+import Button from './evento/Button'
+
 function Evento({numero}) {
 
     function meuEvento(){
         console.log(`opa ${numero}`)
     }
 
+    function segundoEvento() {
+        console.log(`segundo evento`)
+    }
+
     return (
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
