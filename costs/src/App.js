b64759b53e14eb54ffef9222dae3c9cfd8710251
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-21 22:32:18 -0300

    footer e navbar

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,29 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layouts/Container';
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/company">Empresa</Link></li>
-        <li><Link to="/contact">Contato</Link></li>
-        <li><Link to="/newproject">Novo projeto</Link></li>
-      </ul>
-        <Container customClass="min-height">
-          <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route exact path="/company" element={<Company />}/>
-            <Route exact path="/contact" element={<Contact />}/>
-            <Route exact path="/newproject" element={<NewProject />}/>
-          </Routes>
-        </Container>
-      <p>Footer</p>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/projects" element={<Projects />}/>
+          <Route exact path="/company" element={<Company />}/>
+          <Route exact path="/contact" element={<Contact />}/>
+          <Route exact path="/newproject" element={<NewProject />}/>
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
