1749b51580e93b864f48aa9b98a080f32d3a75e8
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-22 11:02:49 -0300

    conectando api com react

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -6,6 +6,9 @@ function Select({text, name, options, handleOnChange, value}) {
             <label forhtml={name}>{text}:</label>
             <select name={name} id={name}>
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
